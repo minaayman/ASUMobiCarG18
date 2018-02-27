0dffac4395fcd4b90641c6f0f7f5444d884092ff
Youstina-magdy <36378252+Youstina-magdy@users.noreply.github.com>
2018-02-27 19:25:59 +0200

    sheet 2 Q4
    push-button

--- AnalogInput.ino
+++ AnalogInput.ino
@@ -1,82 +1,97 @@
-/*
-  Analog Input
 
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
+int analogpin0 = A0;   
+int analogpin1 = A1; 
+int analogpin2 = A2;
+int analogpin3 = A3;
+int analogpin4 = A4;
+int analogpin5 = A5; 
 
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
+int counter=0;
+int buttonstate=0;
+int lastbuttonstate=0;
 
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
 
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInput
-*/
-
-int analogpin0 = A0;    // select the input pin for the potentiometer
-int analogpin1 = A1;    // select the input pin for the potentiometer
-int analogpin2 = A2;    // select the input pin for the potentiometer
-
-int ledPin0 = 13;      // select the pin for the LED
-int ledPin1 = 12;      // select the pin for the LED
-int ledPin2 = 11;      // select the pin for the LED
-
+int switchpin = 2; 
 int sensorValue0=0;
 int sensorValue1=0;
 int sensorValue2=0;
+int sensorValue3=0;
+int sensorValue4=0;
+int sensorValue5=0;
+
+int ledPin0 = 13; 
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
+  pinMode(switchpin, INPUT);
   pinMode(ledPin0, OUTPUT);
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
+
 }
 
 void loop() {
-  // read the value from the sensor:
+  
   sensorValue0 = analogRead(analogpin0);
   sensorValue1 = analogRead(analogpin1);
   sensorValue2 = analogRead(analogpin2);
-
-    if (sensorValue2 > sensorValue1) 
-    {
-
-      
-if(sensorValue2>sensorValue0)
+  sensorValue3 = analogRead(analogpin3);
+  sensorValue4 = analogRead(analogpin4);
+  sensorValue5 = analogRead(analogpin5);
+  buttonstate=digitalRead(switchpin);
+if (buttonstate != lastbuttonstate)
 {
-  digitalWrite(ledPin2, HIGH);
-}
-else
-{
-  digitalWrite(ledPin0, HIGH);
-  }
-
-  } 
-  else 
+  if (buttonstate == HIGH) 
   {
-
-    
-if(sensorValue1 >sensorValue0)
-{
-    digitalWrite(ledPin1, HIGH);
+counter++;
+lastbuttonstate=HIGH;
+  }
+  else
+  {
+    lastbuttonstate=LOW;
+  }
 }
-else
+if(counter==1)
 {
     digitalWrite(ledPin0, HIGH);
+    delay(sensorValue0*1000);
+    digitalWrite(ledPin0, LOW);
 }
+else if(counter==2)
+{
+
+    digitalWrite(ledPin0, HIGH);
+    delay(sensorValue1*1000);
+        digitalWrite(ledPin0, LOW);
+}
+else if(counter==3)
+{
+
+    digitalWrite(ledPin0, HIGH);
+    delay(sensorValue2*1000);
+        digitalWrite(ledPin0, LOW);
+}
+else if(counter==4)
+{
+
+    digitalWrite(ledPin0, HIGH);
+    delay(sensorValue3*1000);
+        digitalWrite(ledPin0, LOW);
+}
+else if(counter==5)
+{
+
+    digitalWrite(ledPin0, HIGH);
+    delay(sensorValue4*1000);
+        digitalWrite(ledPin0, LOW);
+}
+else if(counter==6)
+{
+
+    digitalWrite(ledPin0, HIGH);
+    delay(sensorValue5*1000);
+     digitalWrite(ledPin0, LOW);
+}
+else
+{
+    digitalWrite(ledPin0, LOW);
+
 }
 }
