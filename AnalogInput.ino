41647ac1d0f6ae8ac2be6471e765af4d0c70278b
Youstina-magdy <36378252+Youstina-magdy@users.noreply.github.com>
2018-02-27 19:36:15 +0200

    Update AnalogInput.ino

--- AnalogInput.ino
+++ AnalogInput.ino
@@ -1,32 +1,3 @@
-/*
-  Analog Input
-
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
-
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInput
-*/
-
 int analogpin0 = A0;    // select the input pin for the potentiometer
 int analogpin1 = A1;    // select the input pin for the potentiometer
 int analogpin2 = A2;    // select the input pin for the potentiometer
